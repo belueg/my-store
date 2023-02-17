29f9bb5a95d22dae14ace5855531963fcffa51dd
Belen Gonzalez <belg1294@gmail.com>
2023-02-17 16:35:16 -0300

    adding some get routes

---
+++ data.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    name: 'product1',
+    price: 2300,
+    id: 1,
+    category: 'accommodation'
+  },
+  {
+    name: 'product2',
+    price: 2700,
+    id: 2,
+    category: 'flights'
+  },
+  {
+    name: 'product3',
+    price: 2900,
+    id: 3,
+    category: 'accommodation'
+  }
+]
+const categories = [
+  {
+    id: 1,
+    name: 'accommodation'
+  },
+  {
+    id: 2,
+    name: 'flights'
+  }
+]
+
+module.exports = {
+  products,
+  categories
+}
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
-
+const data = require('./data')
 const app = express()
 const port = 3001
+
+const { categories, products } = data
 
 app.get('/', (req, res) => {
   res.send("Hola bella")
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
 })
+
+
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  const product = products.find(product => product.id === parseInt(id))
+  res.json(product)
+})
+
+app.get('/categories', (req, res) => {
+  res.json(categories)
+})
+
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params
+  const category = categories.find(category => category.id === parseInt(id))
+  res.json(category)
+})
+
+app.get
