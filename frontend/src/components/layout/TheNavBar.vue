52f10cf15183f9476c8c976a50934f2701b0e426
Belen Gonzalez <belg1294@gmail.com>
2023-03-31 19:08:50 -0300

    add navbar and sass dependencies

---
+++ frontend/src/components/layout/TheNavBar.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+
+const menuItems = ref(['Remeras', 'Vestidos', 'Pantalones'])
+
+</script>
+
+<template>
+  <div class="nav">
+    <RouterLink to="/" >Brand</RouterLink>
+    <div class="nav__list">
+      <ul class="nav__list--items">
+        <!-- Reemplazar  <li> por routerLink -->
+        <li class="nav__list--item" v-for="item in menuItems">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.nav {
+  background-color: aquamarine;
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+
+  &__list {
+    width: 50%;
+
+    &--items {
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+    }
+
+    &--item{
+      list-style-type: none;
+    }
+  }
+}
+
+</style>
