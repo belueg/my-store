ad836dea81246ea43619faed54bd0f58068b5731
Belen Gonzalez <belg1294@gmail.com>
2023-03-31 18:20:29 -0300

    add frontend and cors

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const users = ref([])
+
+async function getUser() {
+  try {
+    const { data } = await axios.get('http://localhost:3001/api/users');
+    return data.users
+  } catch (error) {
+    console.error(error);
+  }
+}
+onMounted(async () => {
+  users.value = await getUser()
+
+})
+
+</script>
+
+<template>
+  <main>
+    <h1>{{ users }}</h1>
+
+  </main>
+</template>
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const express = require('express');
 const routerApi = require('./routes/index')
 const { logErrors, errorHandler, boomErrorHandler, validationError } = require('./middlewares/errorHandlers')
+var cors = require('cors')
 
 
 const app = express()
 const port = 3001
 
 app.use(express.json())
+app.use(cors())
 
 routerApi(app)
 
