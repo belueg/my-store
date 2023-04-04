e00a6d0c37b02887ba10c9b92749b26b2b092461
Belen Gonzalez <belg1294@gmail.com>
2023-04-04 17:47:34 -0300

    remove not used icons and create productBox component

---
+++ frontend/src/components/ProductBox.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  product: {
+    type: String,
+    required: true
+  },
+
+})
+</script>
+
+<template>
+  <h2>{{ product.name }}</h2>
+  <img :src="`${product.image}`" />
+  <RouterLink>Ver Producto</RouterLink>
+</template>
+
+<style scoped></style>
