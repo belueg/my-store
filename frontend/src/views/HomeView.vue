cab1099d4199e7d266844764c7a1e660deae0896
Belen Gonzalez <belg1294@gmail.com>
2023-04-12 17:16:50 -0300

    update home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,7 +4,31 @@
 
 <template>
   <main>
-    <h1>Bienvenidos a mi tienda de ropa</h1>
-
+    <div class="home">
+      <h1 class="home__title">Tienda online</h1>
+      <img class="home__img" src="../assets/home-image.avif" width="900" height="400" alt="home image">
+    </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  &__title {
+    margin: 0;
+    margin-top: 140px;
+    margin-bottom: 40px;
+    font-size: 60px;
+    color: black;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.3);
+  }
+
+  &__img {
+    border-radius: 16px;
+  }
+}
+</style>
