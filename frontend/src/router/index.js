6f997a57dfd4b5f088294f930c64f72de68525fc
Belen Gonzalez <belg1294@gmail.com>
2023-05-26 09:44:40 -0300

    frontend routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -20,6 +20,16 @@ const router = createRouter({
       component: () => import('@/views/Cart.vue'),
     },
     {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/Dashboard.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue'),
+    },
+    {
       path: '/clothes/:id/:slug',
       name: 'clothes',
       component: () => import('@/views/Clothes.vue'),
